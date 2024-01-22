92000a3b5fa34e17d859c238fc356e10ec2e1043
Alex de Carvalho <leco_123carvalho@hotmail.com>
2024-01-22 16:06:23 -0300

    6.3. Carregando valos de um Enum
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

---
+++ src/test/java/com/algaworks/junit/utilidade/MultiplicadorTest.java
@@ -0,0 +1,23 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class MultiplicadorTest {
+
+    @ParameterizedTest
+    @EnumSource(value = Multiplicador.class, names = {"DOBRO","TRIPLO"})
+    void aplicarMultiplicador(Multiplicador multiplicador) {
+
+        assertNotNull(multiplicador.aplicarMultiplicador(10.0));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Multiplicador.class)
+    void aplicarMultiplicadorTodosValoreDoEnum(Multiplicador multiplicador) {
+
+        assertNotNull(multiplicador.aplicarMultiplicador(10.0));
+    }
+}
