5f8a8c06d65e16aaa55916257d81512d17895423
Alex de Carvalho <leco_123carvalho@hotmail.com>
2024-01-22 15:49:29 -0300

    6.1. Testes parametrizados
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,6 +72,16 @@ class SaudacaoUtilTest {
 
         // [A]ssert: Validar se o cenário está correto;
         assertDoesNotThrow(chamadaValidaDeMetodo);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints =  {5,6,7,8,9,10,11})
+    public void Dado_horario_matinal_Quando_saudar_Entao_deve_retorna_bom_dia(int hora) {
+
+        String saudacao = saudar(hora);
+
+        // [A]ssert: Validar se o cenário está correto;
+        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
     }
 
 }
