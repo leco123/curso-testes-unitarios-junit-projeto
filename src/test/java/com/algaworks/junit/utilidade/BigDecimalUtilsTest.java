f096017ce62ce8bd84f54c9c4276e0e3e0166090
Alex de Carvalho <leco_123carvalho@hotmail.com>
2024-01-22 15:58:06 -0300

    6.2. Carregados dados de um CSV em um método de teste
    Parametrizando via arquivo CSV
    
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/utilidade/BigDecimalUtilsTest.java
+++ src/test/java/com/algaworks/junit/utilidade/BigDecimalUtilsTest.java
@@ -1,10 +1,12 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BigDecimalUtilsTest {
@@ -19,5 +21,12 @@ class BigDecimalUtilsTest {
         assertTrue(BigDecimalUtils.iguais(x,y));
 
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/numeros.csv")
+    void diferentes(BigDecimal x, BigDecimal y) {
+        assertFalse(BigDecimalUtils.iguais(x,y));
+    }
+
 
 }
